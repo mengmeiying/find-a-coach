0fd2d74a1b9c14015225b91afcdd241aa2411db7
Nadya <meng.meiyingg@gmail.com>
2024-10-08 13:10:08 +0300

    added more router links

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,3 +1,5 @@
 <template>
   <h1>Coach item</h1>
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">Contact</router-link>
 </template>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,6 +3,21 @@
     FILTER
   </section>
   <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
     LIST OF COACHES
+    <ul>
+      <li>Coach
+        <router-link to="/coaches/c1">Coach info</router-link>
+      </li>
+      <li>Coach
+        <router-link to="/coaches/c1">Coach info</router-link>
+      </li>
+      <li>Coach
+        <router-link to="/coaches/c1">Coach info</router-link>
+      </li>
+    </ul>
   </section>
 </template>
