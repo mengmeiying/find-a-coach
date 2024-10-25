88e4430af4c5ae4a5af61e584bc4a35e44e87892
Nadya <meng.meiyingg@gmail.com>
2024-10-25 20:16:52 +0300

    added coaches form component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Register Coach</h1>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+};
+</script>
