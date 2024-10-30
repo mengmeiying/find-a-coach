06ba0f39deed290cec361d0fa2626c5c4287839c
Nadya <meng.meiyingg@gmail.com>
2024-10-30 11:52:11 +0300

    added get request for fetching coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -22,6 +22,34 @@ export default {
 
 
 
-    context.commit('registerCoach', {...coachData, id: userId});
+    context.commit('registerCoach', {
+      ...coachData, 
+      id: userId
+    });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(`https://contact-coach-6ce57-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      //error...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   }
 }
